d78382d0c932827b9ccde24c013b965e74720686
DRA-UPAEP <dra.upaep@gmail.com>
2022-08-27 00:08:56 -0500

    third commit

---
+++ catkin_ws/build/publisher_meta_etapa2/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/cire2022/CIRE2022/catkin_ws/src/publisher_meta_etapa2
+# Build directory: /home/cire2022/CIRE2022/catkin_ws/build/publisher_meta_etapa2
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
